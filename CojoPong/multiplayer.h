bede14f67b6a17d9da90f4e46290dcda3f8484cb
CojocaruSefan <stefan_c97@yahoo.com>
2017-01-05 19:21:25 +0200

    update mplayer
    -paddles se pot misca inainte sa dai space
    -paddle.h schimbat din 100 in 110 ca sa pot imparti paddle in 8 segmente pt coliziune ( 110+5 +5 de la hitbox = 120 ; 120/8=15)

--- CojoPong/multiplayer.h
+++ CojoPong/multiplayer.h
@@ -22,12 +22,12 @@ void multiPlayer()
 	pOnePaddle.x = 40;
 	pOnePaddle.y = 350;
 	pOnePaddle.w = 20;
-	pOnePaddle.h = 100;
+	pOnePaddle.h = 110;
 
 	pTwoPaddle.x = 1140;
 	pTwoPaddle.y = 350;
 	pTwoPaddle.w = 20;
-	pTwoPaddle.h = 100;
+	pTwoPaddle.h = 110;
 
 	ball.w = 20;
 	ball.h = 20;
@@ -58,25 +58,215 @@ void resetBall()
 	xDir = getRandomNumber(4, -4);
 	if ((yDir <= 2 && yDir >= -2) || (xDir <= 2 && xDir >= -2))
 		resetBall();
-
 }
 void SetupRenderer()
 {
 	SDL_RenderSetLogicalSize(gRenderer, 1200, 800);
 	SDL_SetRenderDrawColor(gRenderer, 0, 0, 0, 0);
 }
-bool ballInPOnePaddle()
+
+
+bool segOne_pOne()
+{
+	if (ball.y >= (pOnePaddle.y - 5) && ball.y <= (pOnePaddle.y + 10))
+	{
+		yDir = -4;
+		xDir = 2;
+		return true;
+	}
+	else return false;
+}
+bool segTwo_pOne()
+{
+	if (ball.y > (pOnePaddle.y + 10) && ball.y <= (pOnePaddle.y + 25))
+	{
+		yDir = -4;
+		xDir = 4;
+		return true;
+	}
+	else return false;
+}
+bool segThree_pOne()
+{
+	if (ball.y > (pOnePaddle.y + 25) && ball.y <= (pOnePaddle.y + 30))
+	{
+		yDir = -3;
+		xDir = 4;
+		return true;
+	}
+	else return false;
+}
+bool segFour_pOne()
+{
+	if (ball.y > (pOnePaddle.y + 30) && ball.y <= (pOnePaddle.y + 45))
+	{
+		yDir = -1;
+		xDir = 4;
+		return true;
+	}
+	else return false;
+}
+bool segFive_pOne()
+{
+	if (ball.y > (pOnePaddle.y + 45) && ball.y <= (pOnePaddle.y + 60))
+	{
+		yDir = 1;
+		xDir = 4;
+		return true;
+	}
+	else return false;
+}
+bool segSix_pOne()
+{
+	if (ball.y > (pOnePaddle.y + 60) && ball.y <= (pOnePaddle.y + 75))
+	{
+		yDir = 3;
+		xDir = 4;
+		return true;
+	}
+	else return false;
+}
+bool segSeven_pOne()
+{
+	if (ball.y > (pOnePaddle.y + 75) && ball.y <= (pOnePaddle.y + 90))
+	{
+		yDir = 4;
+		xDir = 3;
+		return true;
+	}
+	else return false;
+}
+bool segEight_pOne()
+{
+	if (ball.y > (pOnePaddle.y + 30) && ball.y <= (pOnePaddle.y + 45))
+	{
+		yDir = 4;
+		xDir = 2;
+		return true;
+	}
+	else return false;
+}
+
+bool ballIn_pOnePaddle()
 {
 	if (
 		ball.x > pOnePaddle.x &&
 		ball.x <= (pOnePaddle.x + pOnePaddle.w) &&
-		(ball.y+ball.h) > (pOnePaddle.y+5) &&
-		ball.y < (pOnePaddle.y + pOnePaddle.h+5) 
+		(ball.y + ball.h) > (pOnePaddle.y + 5) &&
+		ball.y < (pOnePaddle.y + pOnePaddle.h + 5)
 		)
-		return true;
+	{
+		if (segOne_pOne())
+			return true;
+		else
+			if (segTwo_pOne())
+				return true;
+			else
+				if (segThree_pOne())
+					return true;
+				else
+					if (segFour_pOne())
+						return true;
+					else
+						if (segFive_pOne())
+							return true;
+						else
+							if (segSix_pOne())
+								return true;
+							else
+								if (segSeven_pOne())
+									return true;
+								else
+									if (segEight_pOne())
+										return true;
+	}
+		
 	else return false;
 }
-bool ballInPTwoPaddle()
+
+
+bool segOne_pTwo()
+{
+	if (ball.y >= (pTwoPaddle.y - 5) && ball.y <= (pTwoPaddle.y + 10))
+	{
+		yDir = -4;
+		xDir = -2;
+		return true;
+	}
+	else return false;
+}
+bool segTwo_pTwo()
+{
+	if (ball.y > (pTwoPaddle.y + 10) && ball.y <= (pTwoPaddle.y + 25))
+	{
+		yDir = -4;
+		xDir = -4;
+		return true;
+	}
+	else return false;
+}
+bool segThree_pTwo()
+{
+	if (ball.y > (pTwoPaddle.y + 25) && ball.y <= (pTwoPaddle.y + 30))
+	{
+		yDir = -3;
+		xDir = -4;
+		return true;
+	}
+	else return false;
+}
+bool segFour_pTwo()
+{
+	if (ball.y > (pTwoPaddle.y + 30) && ball.y <= (pTwoPaddle.y + 45))
+	{
+		yDir = -1;
+		xDir = -4;
+		return true;
+	}
+	else return false;
+}
+bool segFive_pTwo()
+{
+	if (ball.y > (pTwoPaddle.y + 45) && ball.y <= (pTwoPaddle.y + 60))
+	{
+		yDir = 1;
+		xDir = -4;
+		return true;
+	}
+	else return false;
+}
+bool segSix_pTwo()
+{
+	if (ball.y > (pTwoPaddle.y + 60) && ball.y <= (pTwoPaddle.y + 75))
+	{
+		yDir = 3;
+		xDir = -4;
+		return true;
+	}
+	else return false;
+}
+bool segSeven_pTwo()
+{
+	if (ball.y > (pTwoPaddle.y + 75) && ball.y <= (pTwoPaddle.y + 90))
+	{
+		yDir = 4;
+		xDir = -3;
+		return true;
+	}
+	else return false;
+}
+bool segEight_pTwo()
+{
+	if (ball.y > (pTwoPaddle.y + 30) && ball.y <= (pTwoPaddle.y + 45))
+	{
+		yDir = 4;
+		xDir = -2;
+		return true;
+	}
+	else return false;
+}
+
+bool ballIn_pTwoPaddle()
 {
 	if (
 		ball.x < pTwoPaddle.x &&
@@ -84,9 +274,35 @@ bool ballInPTwoPaddle()
 		(ball.y+ball.h) > (pTwoPaddle.y+5) &&
 		ball.y < (pTwoPaddle.y + pTwoPaddle.h+5)
 		)
-		return true;
+	{
+		if (segOne_pTwo())
+			return true;
+		else
+			if (segTwo_pTwo())
+				return true;
+			else
+				if (segThree_pTwo())
+					return true;
+				else
+					if (segFour_pTwo())
+						return true;
+					else
+						if (segFive_pTwo())
+							return true;
+						else
+							if (segSix_pTwo())
+								return true;
+							else
+								if (segSeven_pTwo())
+									return true;
+								else
+									if (segEight_pTwo())
+										return true;
+	}
 	else return false;
 }
+
+
 bool ballExit()
 {
 	if (ball.x < 1 || ball.x > 1180)
@@ -102,15 +318,13 @@ void ballCollision()
 			yDir -= 2 * yDir;
 
 
-	if (ballInPOnePaddle())
-	{
-		xDir -= 2 * xDir;
+	if (ballIn_pOnePaddle())
+	{		
 		ball.x = pOnePaddle.x + pOnePaddle.w;
 	}
 	else
-		if (ballInPTwoPaddle())
-		{
-			xDir -= 2 * xDir;
+		if (ballIn_pTwoPaddle())
+		{		
 			ball.x = pTwoPaddle.x - ball.w;
 		}
 }
@@ -142,57 +356,55 @@ void RunGame()
 			}
 		}
 			
-				const Uint8* currentKeyStates = SDL_GetKeyboardState(NULL);
-				if (gameStart == false)
-				{
-					if (currentKeyStates[SDL_SCANCODE_SPACE])
+			const Uint8* currentKeyStates = SDL_GetKeyboardState(NULL);
+			if (gameStart == false)
+			{
+				if (currentKeyStates[SDL_SCANCODE_SPACE])
 						gameStart = true;
+			}	
+			if (currentKeyStates[SDL_SCANCODE_W] && currentKeyStates[SDL_SCANCODE_UP])
+			{
+				pOnePaddle.y -= 5;
+				pTwoPaddle.y -= 5;	
+			}
+			else
+				if (currentKeyStates[SDL_SCANCODE_S] && currentKeyStates[SDL_SCANCODE_DOWN])
+				{
+					pOnePaddle.y += 5;
+					pTwoPaddle.y += 5;	
 				}
 				else
-				{
-					if (currentKeyStates[SDL_SCANCODE_W] && currentKeyStates[SDL_SCANCODE_UP])
+					if (currentKeyStates[SDL_SCANCODE_W] && currentKeyStates[SDL_SCANCODE_DOWN])
 					{
 						pOnePaddle.y -= 5;
-						pTwoPaddle.y -= 5;	
+						pTwoPaddle.y += 5;
 					}
 					else
-						if (currentKeyStates[SDL_SCANCODE_S] && currentKeyStates[SDL_SCANCODE_DOWN])
+						if (currentKeyStates[SDL_SCANCODE_S] && currentKeyStates[SDL_SCANCODE_UP])
 						{
 							pOnePaddle.y += 5;
-							pTwoPaddle.y += 5;	
+							pTwoPaddle.y -= 5;		
 						}
 						else
-							if (currentKeyStates[SDL_SCANCODE_W] && currentKeyStates[SDL_SCANCODE_DOWN])
+						{
+							if (currentKeyStates[SDL_SCANCODE_UP])
 							{
-								pOnePaddle.y -= 5;
-								pTwoPaddle.y += 5;
+								pTwoPaddle.y -= 5;	
 							}
-							else
-								if (currentKeyStates[SDL_SCANCODE_S] && currentKeyStates[SDL_SCANCODE_UP])
-								{
-									pOnePaddle.y += 5;
-									pTwoPaddle.y -= 5;		
-								}
-								else
-								{
-									if (currentKeyStates[SDL_SCANCODE_UP])
-									{
-										pTwoPaddle.y -= 5;	
-									}
-									else if (currentKeyStates[SDL_SCANCODE_DOWN])
-									{
-										pTwoPaddle.y += 5;	
-									}
-									if (currentKeyStates[SDL_SCANCODE_W])
-									{
-										pOnePaddle.y -= 5;	
-									}
-									else if (currentKeyStates[SDL_SCANCODE_S])
-									{
-										pOnePaddle.y += 5;
-									}
-								}
-				}
+							else if (currentKeyStates[SDL_SCANCODE_DOWN])
+							{
+								pTwoPaddle.y += 5;	
+							}
+							if (currentKeyStates[SDL_SCANCODE_W])
+							{
+								pOnePaddle.y -= 5;	
+							}
+							else if (currentKeyStates[SDL_SCANCODE_S])
+							{
+								pOnePaddle.y += 5;
+							}
+						}
+				
 			
 		
 		if (pOnePaddle.y < 1)
